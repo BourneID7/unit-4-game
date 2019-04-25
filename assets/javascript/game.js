634a56b85611db06d13e39d638ff75b23d9ae054
Jason Bourne <m.bourne@wi.rr.com>
2019-04-25 06:03:12 -0500

    got accumulating score display working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,108 @@
 $(document).ready(function() {
+        
+        // global variables
 
-    // global variables
+        // Empty variable to hold random number to match
+        var num = 0;
 
-    // Empty variable to hold random number to match
-    var num = "";
+        // variables to hold even or odd crystal values
+        var even = [2, 4, 6, 8, 10, 12];
+        var odd = [1, 3, 5, 7, 9, 11];
 
-    // variables to hold even or odd crystal values
-    var even = [2, 4, 6, 8, 10, 12];
-    var odd = [1, 3, 5, 7, 9, 11];
+        // Empty variables to hold the random value for each crystal
+        var ruby = "";
+        var sapphire = "";
+        var diamond = "";
+        var amethyst = "";
 
-    // Empty variables to hold the random value for each crystal
-    var ruby = "";
-    var sapphire = "";
-    var diamond = "";
-    var amethyst = "";
+        //Empty variable to hold running total of clicked crystal values
+        var score = 0;
+        var scoreText = $("#yourScore");
 
-    //Empty variable to hold running total of clicked crystal values
-    var score = 0;
-    var scoreText = $("#yourScore");
+        //Empty variables to hold wins/losses
+        var wins = 0;
+        var losses = 0;
 
-    //Empty variables to hold wins/losses
-    var wins = 0;
-    var losses = 0;
-
-
-    // $(document).keyup(function newGame() {
-    //     newGame();
-
+    function newGame() {
 
         // generate random number between 19 and 120
-        var num = 19 + Math.floor(Math.random() * 120);
+        num = 19 + Math.floor(Math.random() * 120);
         console.log("random number to match is " + num);
 
         // display number to match
         $("#matchNumber").append("The number to match is " + num);
 
         //generate even or odd random number between 1 and 12 for each crystal
-        var ruby = even[Math.floor(Math.random() * even.length)];
+        ruby = even[Math.floor(Math.random() * even.length)];
         console.log("random value of ruby is " + ruby);
 
-        var sapphire = odd[Math.floor(Math.random() * odd.length)];
+        sapphire = odd[Math.floor(Math.random() * odd.length)];
         console.log("random value of sapphire is " + sapphire);
 
-        var diamond = odd[Math.floor(Math.random() * odd.length)];
+        diamond = odd[Math.floor(Math.random() * odd.length)];
         console.log("random value of diamond is " + diamond);
 
-        var amethyst = even[Math.floor(Math.random() * even.length)];
+        amethyst = even[Math.floor(Math.random() * even.length)];
         console.log("random value of amethyst is " + amethyst);
 
         // assign crystal values to corresponding image. Function to add value to score when clicked.
+
         $("#sapphireBtn").on("click", function() {
             $(this).sapphire;
             score = score + sapphire;
             console.log("current score is " + score);
+
+            // update display score
+            scoreText.text(null);
+            $("#yourScore").append(score);
+
         })
         $("#rubyBtn").on("click", function() {
             $(this).ruby;
             score = score + ruby;
             console.log("current score is " + score);
+
+            // update display score
+            scoreText.text(null);
+            $("#yourScore").append(score);
+
         })
         $("#diamondBtn").on("click", function() {
             $(this).diamond;
             score = score + diamond;
             console.log("current score is " + score);
+
+            // update display score
+            scoreText.text(null);
+            $("#yourScore").append(score);
+
         })
         $("#amethystBtn").on("click", function() {
             $(this).amethyst;
             score = score + amethyst;
             console.log("current score is " + score);
+
+            // update display score
+            scoreText.text(null);
+            $("#yourScore").append(score);
+
         })
-
-        // display score
-        $(scoreText).text("Your score is: " + score);
-
-
+    
         // determine win. Add 1 to total wins & display win message
         if (score === num) {
             wins++;
-            $("#winMessage").append("You win!");
+            $("#winMessage").append("Game over. You win!");
         } 
         // determine loss. Add 1 to total losses & display loss message
         else if (score > num) {
             losses++;
-            $("#lossMessage").append("Sorry! You lost!");
+            $("#lossMessage").append("Game over. Sorry, You lost!");
         }
+       
+    }
+    newGame();
+    
+    // function to reset game with new number to match, crystal values & clear score
 
-
-        // function to reset game with new number to match, crystal values & clear score
-
-
-    // })
 
 })
